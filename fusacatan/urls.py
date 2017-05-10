e33b05bb2ad32f5ca2b422ef3b7f9add43969340
Jhonbeltran <jbeltranleon@gmail.com>
2017-05-10 16:56:34 -0500

    Actualizo la pagina principal

--- fusacatan/urls.py
+++ fusacatan/urls.py
@@ -1,25 +1,13 @@
-"""fusacatan URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from material.frontend import urls as frontend_urls
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls, name='admin'),
+    url(r'^$', TemplateView.as_view(template_name="home.html"),
+        name="home"),
     # url(r'^', include('zones.urls', namespace='zones')),
 ] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
