d90e6c5fba3858384b4096807dc8985e943ccef1
Jhonbeltran <jbeltranleon@gmail.com>
2017-05-10 17:09:08 -0500

    Renombro archivos

--- fusacatan/urls.py
+++ fusacatan/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^$', TemplateView.as_view(template_name="home.html"),
         name="home"),
-    # url(r'^', include('zones.urls', namespace='zones')),
-] # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^', include('zones.urls', namespace='zones')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- zones/views.py
+++ zones/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import UpdateView
+from django.views.generic.edit import DeleteView
+from .mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse_lazy
+from .models import Zone
+from .forms import EventForm
 
-# Create your views here.
+class EventCreate(LoginRequiredMixin, CreateView):
+    model = Zone
+    success_url = reverse_lazy('zones:zone')
+    fields = ['date', 'location', 'moisture',
+    	      'rainfall', 'trees']
+
+class EventList(ListView):
+    model = Zone
+
+class EventDetail(LoginRequiredMixin, DetailView):
+    model = Zone
+
+class EventUpdate(LoginRequiredMixin, UpdateView):
+    model = Zone
+    success_url = reverse_lazy('zones:zone')
+    fields = ['date', 'location', 'moisture',
+    	      'rainfall', 'trees']
+
+
+class EventDelete(LoginRequiredMixin, DeleteView):
+    model = Zone
+    success_url = reverse_lazy('zones:zone')
