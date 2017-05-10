526ba2a2b4dc8e7790652a439137c4bc783e3458
Jhonbeltran <jbeltranleon@gmail.com>
2017-05-10 15:31:49 -0500

    Agrego modelo zones

--- zones/models.py
+++ zones/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Zone(models.Model):
+    date = models.DateTimeField(default=datetime.now)
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    soil = models.CharField(max_length=255)
+    moisture = models.DecimalField(max_digits=6, decimal_places=2)
+    rainfall = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('date',)
+
